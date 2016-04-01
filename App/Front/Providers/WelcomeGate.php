eb2c8c1bcb041aba9cd05b50f6be59c3b323204a
Brandon Braner <brandon.braner@ave81.com>
2016-04-01 15:47:48 -0500

    added no cache to header of redirect

--- App/Front/Providers/WelcomeGate.php
+++ App/Front/Providers/WelcomeGate.php
@@ -40,6 +40,8 @@ class WelcomeGate
     }
 
     protected function welcomeGateHttpRedirect(){
+        header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+        header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // past date to encourage expiring immediately
         header( 'Location: ' . $this->welcomeGateUrl, true, 307 );
         die();
     }
