b8904c2f0663659dea44cf46e0a7641a4ac3b35e
Brandon Braner <brandon.braner@ave81.com>
2016-04-15 12:12:49 -0500

    added a config array for testing on dark

--- App/Config/App.php
+++ App/Config/App.php
@@ -1,24 +1,54 @@
 <?php
 
-$config = array(
+$test = true;
 
-    /*
-  |--------------------------------------------------------------------------
-  | Base Path
-  |--------------------------------------------------------------------------
-  |
-  | Base path setup for a wordpress plugin
-  |
-  */
-  'basePath' => plugin_dir_path(dirname(dirname(__FILE__))),
-  'api'      => array(
-    'sessions' => array(
-      'new'     => 'https://api.leadpages.io/auth/v1/sessions/',
-      'current' => 'https://api.leadpages.io/auth/v1/sessions/current'
-    ),
-    'pages'    => 'https://my.leadpages.net/page/v1/pages'
-  )
-);
+if($test){
+    $currentTestBranchUrl = "https://as-382-dot-leadpage-test.appspot.com";
+    $config = array(
 
-$config['admin_assets'] = plugin_dir_url((dirname(__FILE__))) . 'assets';
-$config['admin_images'] = $config['admin_assets'] . '/images';
+        /*
+      |--------------------------------------------------------------------------
+      | Base Path
+      |--------------------------------------------------------------------------
+      |
+      | Base path setup for a wordpress plugin
+      |
+      */
+      'basePath' => plugin_dir_path(dirname(dirname(__FILE__))),
+      'api'      => array(
+        'sessions' => array(
+          'new'     => 'https://stargate-dot-leadpage-test.appspot.com/auth/v1/sessions',
+          'current' => 'https://stargate-dot-leadpage-test.appspot.com/auth/v1/sessions/current'
+        ),
+        'pages'    => $currentTestBranchUrl.'/page/v1/pages',
+        'leadboxes' => $currentTestBranchUrl."//leadbox/v1/leadboxes"
+      )
+    );
+
+    $config['admin_assets'] = plugin_dir_url((dirname(__FILE__))) . 'assets';
+    $config['admin_images'] = $config['admin_assets'] . '/images';
+}else{
+    $config = array(
+
+        /*
+      |--------------------------------------------------------------------------
+      | Base Path
+      |--------------------------------------------------------------------------
+      |
+      | Base path setup for a wordpress plugin
+      |
+      */
+      'basePath' => plugin_dir_path(dirname(dirname(__FILE__))),
+      'api'      => array(
+        'sessions' => array(
+          'new'     => 'https://api.leadpages.io/auth/v1/sessions/',
+          'current' => 'https://api.leadpages.io/auth/v1/sessions/current'
+        ),
+        'pages'    => 'https://my.leadpages.net/page/v1/pages'
+      )
+    );
+
+    $config['admin_assets'] = plugin_dir_url((dirname(__FILE__))) . 'assets';
+    $config['admin_images'] = $config['admin_assets'] . '/images';
+}
+
