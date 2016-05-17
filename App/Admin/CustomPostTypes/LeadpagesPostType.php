ebd8480d8d87848bbe0047f93c7ddf801be54652
Brandon Braner <brandon.braner@ave81.com>
2016-05-17 14:03:37 -0500

    fixed LeadPages to Leadpages

--- App/Admin/CustomPostTypes/LeadpagesPostType.php
+++ App/Admin/CustomPostTypes/LeadpagesPostType.php
@@ -22,14 +22,14 @@ class LeadpagesPostType extends CustomPostType implements CustomPostTypeColumns
     public function defineLabels()
     {
         $this->labels = array(
-          'name'               => _x('LeadPages', 'post type general name'),
-          'singular_name'      => _x('LeadPage', 'post type singular name'),
+          'name'               => _x('Leadpages', 'post type general name'),
+          'singular_name'      => _x('Leadpage', 'post type singular name'),
           'add_new'            => _x('Add New', 'leadpage'),
-          'add_new_item'       => __('Add New LeadPage'),
-          'edit_item'          => __('Edit LeadPage'),
-          'new_item'           => __('New LeadPage'),
-          'view_item'          => __('View LeadPages'),
-          'search_items'       => __('Search LeadPages'),
+          'add_new_item'       => __('Add New Leadpage'),
+          'edit_item'          => __('Edit Leadpage'),
+          'new_item'           => __('New Leadpage'),
+          'view_item'          => __('View Leadpages'),
+          'search_items'       => __('Search Leadpages'),
           'not_found'          => __('Nothing found'),
           'not_found_in_trash' => __('Nothing found in Trash'),
           'parent_item_colon'  => ''
@@ -41,7 +41,7 @@ class LeadpagesPostType extends CustomPostType implements CustomPostTypeColumns
         global $config;
         $this->args   = array(
           'labels'               => $this->labels,
-          'description'          => 'Allows you to have LeadPages on your WordPress site.',
+          'description'          => 'Allows you to have Leadpages on your WordPress site.',
           'public'               => true,
           'publicly_queryable'   => true,
           'show_ui'              => true,
