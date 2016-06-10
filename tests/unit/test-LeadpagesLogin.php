849ab505ef23b18e4168390e3d554df26c558a6d
Brandon Braner <brandon.braner@ave81.com>
2016-06-10 15:42:12 -0500

    setup tests

--- tests/unit/test-LeadpagesLogin.php
+++ tests/unit/test-LeadpagesLogin.php
@@ -1,19 +1,31 @@
 <?php
 
-use Leadpages\ServiceProviders\LeadpagesLogin;
+use LeadpagesWP\ServiceProviders\WordPressLeadpagesAuth;
 
 class Test_LeadpagesLogin_Success extends WP_UnitTestCase
 {
 
     public $leadpagesLogin;
+    public $stub;
 
     public function setUp()
     {
         parent::setUp();
         //setup leadpages login object
-        //remind app is setup in parent::setUp();
-        $this->leadpagesLogin = $this->app['leadpagesLogin'];
-        $this->assertInstanceOf(LeadpagesLogin::class, $this->leadpagesLogin);
+        $this->stub = $this->getMockBuilder(WordPressLeadpagesAuth::class)
+            ->setConstructorArgs([new GuzzleHttp\Client()])
+            ->setMethods(['getToken'])
+            ->getMock();
+
+        $this->stub->token = getenv('testToken');
+
+        $this->stub->expects($this->any())
+                   ->method('getToken')
+                   ->will($this->returnValue($this->stub->token));
+
+
+        //set to true to simulate getting back a true response from api call
+
 
     }
 
@@ -24,35 +36,51 @@ class Test_LeadpagesLogin_Success extends WP_UnitTestCase
         unset($this->leadpagesLogin->token);
     }
 
+
     /**
-     * Get the users token and ensure that it is set in the response and get set in the database properly
+     * checks to see if $this->token is empty when it is fetched from the database
      *
      * @group login_success
      */
-
-    public function test_get_user_token_success()
+    public function test_check_if_token_is_empty()
     {
-        // call leadpages login to getUserToken
-        $response = $this->leadpagesLogin->getUserToken($this->goodUserName, $this->goodPassword);
-        //make sure we veirfy it is a success
-        //this also stores the token in the database
-        $this->assertEquals('success', $response['responseCode']);
-        $this->assertNotEmpty($this->leadpagesLogin->token, 'Leadpages Security Token Not Set');
+        $result = $this->stub->checkIfTokenIsEmpty();
+        $error = $result['error'];
+        $code = $result['code'];
 
-        //grab the database token and ensure that it matches what was returned, also ensures it was set
-        $dbToken = get_option('leadpages_security_token');
-        $this->assertEquals($this->leadpagesLogin->token, $dbToken);
+        $this->assertFalse($error);
+        $this->assertEquals('200', $code);
     }
 
     /**
      * User should be logged in so we can ensure that their token is in the database
      *
-     * group login_success
+     * @group login_success
      */
     public function test_user_is_logged_in_success()
     {
-        $isLoggedIn = $this->leadpagesLogin->isUserLoggedIn();
-        $this->assertTrue($isLoggedIn);
+        //stub out $this->getToken above to return a token preset in env.
+        //makes actually call
+        $result = $this->stub->isLoggedIn();
+        $this->assertTrue($result);
+    }
+
+    /**
+     * User should be able to login
+     *
+     * @group login_success
+     */
+
+    public function test_login()
+    {
+        //preset post variables
+        $_POST['username'] = sanitize_text_field(getenv('username'));
+        $_POST['password'] = sanitize_text_field(getenv('password'));
+
+        $response = $this->stub->login();
+
+        $this->assertEquals('success', $response);
+
     }
 
 }
@@ -62,47 +90,77 @@ class Test_LeadpagesLogin_Fail extends WP_UnitTestCase
 {
 
     public $leadpagesLogin;
+    public $stub;
 
     public function setUp()
     {
         parent::setUp();
         //setup leadpages login object
-        //remind app is setup in parent::setUp();
-        $this->leadpagesLogin = $this->app['leadpagesLogin'];
-        $this->assertInstanceOf(LeadpagesLogin::class, $this->leadpagesLogin);
+        $this->stub = $this->getMockBuilder(WordPressLeadpagesAuth::class)
+                           ->setConstructorArgs([new GuzzleHttp\Client()])
+                           ->setMethods(['getToken'])
+                           ->getMock();
+
+        $this->stub->expects($this->any())
+                   ->method('getToken')
+                   ->will($this->returnValue($this->stub->token));
+
+
+        //set to true to simulate getting back a true response from api call
+
 
     }
 
     public function tearDown()
     {
         parent::tearDown();
+        //unset token after every test so it can be retested
+        unset($this->leadpagesLogin->token);
     }
 
 
     /**
-     * we Should nto get a token back and should get a 401 error
+     * checks to see if $this->token is empty when it is fetched from the database
      *
-     * @group login_fail
+     * @group login-fail
      */
-
-    public function test_get_user_token_fail()
+    public function test_check_if_token_is_empty_fail()
     {
-        $response = $this->leadpagesLogin->getUserToken($this->badUserName, $this->badPassword);
-        $this->assertNotEquals('success', $response['responseCode']);
-        $this->assertEquals('401', $response['responseCode']);
-        $this->assertNull($this->leadpagesLogin->token, 'Leadpages Security Token Not Set');
+        $result = $this->stub->checkIfTokenIsEmpty();
+        $error = $result['error'];
+        $code = $result['code'];
+
+        $this->assertTrue($error);
+        $this->assertEquals('500', $code);
     }
 
     /**
-     * Check to make sure user is not logged in by checking database for a token
+     * User should be logged in so we can ensure that their token is in the database
      *
-     * @group login_fail
+     * @group login-fail
      */
-
     public function test_user_is_logged_in_fail()
     {
-        $isLoggedIn = $this->leadpagesLogin->isUserLoggedIn();
-        $this->assertFalse($isLoggedIn);
+        //stub out $this->getToken above to return a token preset in env.
+        //makes actually call
+        $result = $this->stub->isLoggedIn();
+        $this->assertFalse($result);
+    }
+
+    /**
+     * @group login-fail
+     */
+    public function test_login_fail()
+    {
+        //preset post variables
+        $_POST['username'] = 'unittest@wordpressplugintest.com';
+        $_POST['password'] = 'example';
+
+        $response = $this->stub->login();
+        $response = json_decode($response, true);
+
+        $this->assertEquals('401', $response['code']);
+
     }
 
 }
