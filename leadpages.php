8b509010cd87878cc114758b093e311620001586
Brandon Braner <brandon.braner@ave81.com>
2016-06-27 12:58:02 -0500

    added version number to check against to revert changes. Did this as I think it will be quicker than using get_plugin_data

--- leadpages.php
+++ leadpages.php
@@ -5,27 +5,30 @@ Plugin Name: Leadpages Connector
 Plugin URI: http://wordpress.org/plugins/hello-dolly/
 Description:
 Author: Leadpages
-Version: 2.0
+Version: 2.0.2
 Author URI: http://leadpages.net
 */
 
+$plugin_version = '2.0.2';
 
-/*
-  |--------------------------------------------------------------------------
-  | Application Entry Point
-  |--------------------------------------------------------------------------
-  |
-  | This will be your plugin entry point. This file should
-  | not contain any logic for your plugin what so ever.
-  |
-  */
+  /*
+    |--------------------------------------------------------------------------
+    | Application Entry Point
+    |--------------------------------------------------------------------------
+    |
+    | This will be your plugin entry point. This file should
+    | not contain any logic for your plugin what so ever.
+    |
+    */
 
 
 require_once('vendor/autoload.php');
 require_once('App/Config/App.php');
-require_once $leadpagesConfig['basePath'] . 'Framework/ServiceContainer/ServiceContainer.php';
+require_once($leadpagesConfig['basePath'] . 'Framework/ServiceContainer/ServiceContainer.php');
 require_once($leadpagesConfig['basePath'].'App/Config/RegisterProviders.php');
 
+//fix broken stuff in 2.0 and 2.0.x
+require_once($leadpagesConfig['basePath'].'App/Lib/RevertChanges.php');
 
 
 /*
