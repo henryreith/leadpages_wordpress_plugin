e4f5fe44e689bbc79f19b1b0e54940bc3da616a9
Brandon Braner <brandon.braner@ave81.com>
2016-06-27 12:58:55 -0500

    wrapped everything inside a document ready also added some commented out code to hide the slug on certain page types though not sure we need to do that

--- App/assets/js/LeadpagesPostType.js
+++ App/assets/js/LeadpagesPostType.js
@@ -1,31 +1,51 @@
 (function ($) {
 
-    function getLeadPages(){
+    $( document ).ready( function() {
 
-        //var loading ='<div class="ui-loading"> <div class="ui-loading__dots ui-loading__dots--1"></div> <div class="ui-loading__dots ui-loading__dots--2"></div> <div class="ui-loading__dots ui-loading__dots--3"></div> </div>';
+        function getLeadPages(){
 
-        $.ajax({
-            type: 'POST',
-            url: ajax_object.ajax_url,
-            data: {
-                action: 'get_pages_dropdown',
-                id: ajax_object.id
-            },
-            beforeSend: function (data) {
+            //var loading ='<div class="ui-loading"> <div class="ui-loading__dots ui-loading__dots--1"></div> <div class="ui-loading__dots ui-loading__dots--2"></div> <div class="ui-loading__dots ui-loading__dots--3"></div> </div>';
 
-            },
-            success: function (response) {
-                $(".ui-loading").hide();
-                $(".leadpagesSlug").show();
-                $(".leadpageType").show();
-                $(".leadpagesSelect").show();
-              $("#leadpages_my_selected_page").append(response);
-            }
-        });
-    }
+            $.ajax({
+                type: 'POST',
+                url: ajax_object.ajax_url,
+                data: {
+                    action: 'get_pages_dropdown',
+                    id: ajax_object.id
+                },
+                beforeSend: function (data) {
+
+                },
+                success: function (response) {
+                    $(".ui-loading").hide();
+                    $(".leadpagesSlug").show();
+                    $(".leadpageType").show();
+                    $(".leadpagesSelect").show();
+                    $("#leadpages_my_selected_page").append(response);
+                }
+            });
+        }
+
+        getLeadPages();
 
 
+        //not sure we need to do this
+    //    var $body = $('body');
+    //
+    //    function hideSlugFor404andHome(){
+    //        var pageType = $("#leadpageType").val();
+    //        if(pageType == 'nf' || pageType == 'fp'){
+    //            $("#leadpage-slug").hide();
+    //        }else{
+    //            $("#leadpage-slug").show();
+    //        }
+    //    }
+    //
+    //    $body.on('change', '#leadpageType', function(){
+    //        hideSlugFor404andHome();
+    //    });
+    //
+    //
 
-    getLeadPages();
-
+    });
 }(jQuery));
