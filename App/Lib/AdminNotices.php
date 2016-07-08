896ae7b62b68b0dc5f40c97c87b6765fd6793297
Brandon Braner <brandon.braner@ave81.com>
2016-07-08 09:58:37 -0500

    added notice to notice library

--- App/Lib/AdminNotices.php
+++ App/Lib/AdminNotices.php
@@ -24,6 +24,15 @@ BOM;
         <?php
     }
 
+    public static function TurnOnPermalinks()
+    {
+        ?>
+        <div class="notice notice-error is-dismissible">
+            <p><?php _e( 'LeadPages plugin needs <a style="text-decoration: underline"href="options-permalink.php">permalinks</a> enabled!', 'leadpages' ); ?></p>
+        </div>
+        <?php
+    }
+
 
 
 }
