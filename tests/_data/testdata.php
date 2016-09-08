917d1173aa4e699657e4d170c17e010947ee8d99
rapidology <brandon.braner@ave81.com>
2016-09-08 14:29:15 -0500

    add test data file

---
+++ tests/_data/testdata.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+
+  'leadpagesUsername' => getenv('username'),
+  'leadpagesPassword' => getenv('password'),
+  'adminUsername'     => getenv('adminUsername'),
+  'adminPassword'     => getenv('adminPassword'),
+
+);
